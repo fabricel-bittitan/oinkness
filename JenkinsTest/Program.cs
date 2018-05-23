106cbf7c3b24adb783bbcbaf411f6d2000cc02e3
FabriceLing <fabricel@bittitan.com>
2018-05-23 11:55:39 -0700

    First push of silly .NET app.

---
+++ JenkinsTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JenkinsTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
